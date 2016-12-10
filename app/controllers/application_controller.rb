a3019cc5cdc8a3d104422cdb0813c48dbcce3040
Sachin Karia <sachin.karia01@gmail.com>
2016-12-10 23:28:32 +0000

    working app

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,4 @@
+class ApplicationController < ActionController::Base
+  include DeviseTokenAuth::Concerns::SetUserByToken
+  respond_to :json
+end
---
+++ config/application.rb
@@ -0,0 +1,47 @@
+require_relative 'boot'
+
+require "rails"
+# Pick the frameworks you want:
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "action_cable/engine"
+require "sprockets/railtie"
+# require "rails/test_unit/railtie"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module RentooAuth
+  class Application < Rails::Application
+    config.active_record.raise_in_transactional_callbacks = true
+
+
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
+
+        resource '/cors',
+        :headers => :any,
+        :methods => [:post],
+        :credentials => true,
+        :max_age => 0
+
+        resource '*',
+        :headers => :any,
+        :methods => [:get, :post, :delete, :put, :patch, :head],
+        :max_age => 0,
+        :expose => ['access-token','token-type','expiry','client','uid', 'Content-Type']
+      end
+    end
+
+    config.api_only = true
+    config.middleware.use ActionDispatch::Flash
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
+  end
+end
